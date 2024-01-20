e468eb3302a57a54a8e714540add360c3acfdf92
sirenlacys <151232625+sirenlacys@users.noreply.github.com>
2024-01-20 12:19:42 -0800

    Add files via upload

--- sirenlacys.js
+++ sirenlacys.js
@@ -1,38 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     var videoContainer = document.getElementById('videoContainer');
     var searchInput = document.getElementById('searchInput');
-
-    // Mengumpulkan elemen video
-    var videos = videoContainer.querySelectorAll('video');
+    var videos = videoContainer.querySelectorAll('.video-wrapper');
 
     searchInput.addEventListener('input', function () {
         var searchQuery = searchInput.value.toLowerCase();
 
         videos.forEach(function (video) {
-            var title = video.dataset.title.toLowerCase();
+            var titleElement = video.querySelector('.video-title');
+            var title = titleElement.textContent.toLowerCase();
 
             if (title.includes(searchQuery)) {
                 video.style.display = 'block';
+                // Sorot kata kunci pencarian
+                titleElement.innerHTML = highlightSearchTerm(title, searchQuery);
             } else {
                 video.style.display = 'none';
             }
         });
     });
 
+    function highlightSearchTerm(text, searchTerm) {
+        var highlightedText = text.replace(new RegExp(searchTerm, 'gi'), function (match) {
+            return '<span class="highlight">' + match + '</span>';
+        });
+        return highlightedText;
+    }
+
     // Menanggapi acara play pada setiap video
     videos.forEach(function (video) {
-        video.addEventListener('play', function () {
+        video.querySelector('video').addEventListener('play', function () {
             // Menghentikan pemutaran video lain jika sedang diputar
             videos.forEach(function (otherVideo) {
-                if (otherVideo !== video && !otherVideo.paused) {
-                    otherVideo.pause();
+                if (otherVideo !== video && otherVideo.querySelector('video').paused === false) {
+                    otherVideo.querySelector('video').pause();
                 }
             });
-
-            // Menampilkan judul video yang sedang diputar
-            var title = video.dataset.title;
-            var videoTitleElement = document.querySelector('.video-title');
-            videoTitleElement.textContent = title;
         });
     });
 });
